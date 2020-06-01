527ec91e1ba21086806319b6685556f90a3b5df0
furawa <frankenprofessional@gmail.com>
2020-06-01 16:15:46 +0200

    feat: Compute the growth rate

--- 02_analyze_company_sales/analyze_sales.py
+++ 02_analyze_company_sales/analyze_sales.py
@@ -3,7 +3,6 @@
 
 # # ANALYZE DATA
 # import the cleaned datasets
-from cleaning_datasets import sales_clean
 # Load all the needed packages
 import pandas as pd
 import numpy as np
@@ -12,19 +11,20 @@ import seaborn as sns
 import datetime
 import scipy.stats as sci
 import time
+import statsmodels.api as sm
 # get_ipython().run_line_magic('matplotlib', 'inline')
 sns.set_palette("colorblind")
 sns.set_context("talk")
 sns.set(style = "white", rc={'figure.figsize':(11.7,8.27)})
 
-# ## Import The Dataset
-# Import the cleaned dataset
-# sales_clean = pd.read_csv("clean_datasets/sales_clean.csv", 
-#                           dtype={"category":"category", "gender":"category"},
-#                           parse_dates=["date"])
-# sales_clean.info()
+## Import The Dataset
+print("Import the cleaned dataset for the analysis")
+sales_clean = pd.read_csv("clean_datasets/sales_clean.csv", 
+                          dtype={"category":"category", "gender":"category"},
+                          parse_dates=["date"])
+print("sales dataframe: \n", sales_clean)
 
-# ## Summary of the Sales
+print("Summary of the Sales:\n")
 
 # Total sales since the beginning
 total_sales = sales_clean.price.sum()
@@ -43,7 +43,7 @@ and just in January and February 2022 the total sale is ${:,}. This means that i
                                                                     total_products, total_transactions))
 
 
-# ## Univariate Analysis 
+print("Univariate Analysis") 
 # 
 # ### **What is the Average Price of the Products?**
 sales_clean[sales_clean.date == sales_clean.date.min()]
@@ -60,7 +60,7 @@ iqr(sales_clean.price)
 
 # We have an IQR of around **10**, the variability is high. Let's make a plot.
 
-# Boxplot to see the variability
+print("Boxplot to see the variability")
 sns.boxplot("price", data = sales_clean)
 plt.title("Variability of the Prices", fontsize = 24)
 plt.xlabel("Price ($)", fontsize = 18, fontweight = "bold", fontstyle = "italic")
@@ -77,9 +77,9 @@ outliers = sales_clean.price.quantile(0.75)+1.5*iqr(sales_clean.price)
 sales_clean.price[sales_clean.price > outliers].sort_values()
 
 
-# All the price between **34.77** to **300** are outliers. 
+print("All the price between **34.77** to **300** are outliers.") 
 
-# Distribution of the non outliers values
+print("Distribution of the non outliers values")
 sales_clean_no_outliers = sales_clean[sales_clean.price < outliers] # Compute the dataframe of the non outliers
 bins = int(round(1 + np.log2(sales_clean_no_outliers.shape[0]))) # Optimal number of bins
 sns.distplot(sales_clean_no_outliers.price, bins = bins) # Distribution plot of the non outliers values
@@ -96,15 +96,15 @@ time.sleep(2)
 sales_clean_no_outliers.price.agg([np.mean, np.median])
 
 
-# If we remove all the outliers the mean and median are almost the same and are around $13. 
+print("If we remove all the outliers the mean and median are almost the same and are around $13.") 
 
-# check the proportion of outliers in the dataframe
+print("check the proportion of outliers in the dataframe")
 sales_clean_outliers = sales_clean.price[sales_clean.price > outliers]
 prop_outliers = len(sales_clean_outliers) / len(sales_clean) * 100
 prop_outliers
 
 
-# The outliers represent about **6%** of the number of sales. Let see what it represent as value
+#The outliers represent about **6%** of the number of sales. Let see what it represent as value
 
 
 # total sales for the outliers
@@ -112,17 +112,17 @@ outliers_total_sales = sales_clean_outliers.sum() / sales_clean.price.sum() * 10
 outliers_total_sales
 
 
-# Even if the outliers represent just **6%** of the sales, they represent **25%** of the total purchases amount.
-
-# mean, median and mode of the price in the dataframe
+print("Even if the outliers represent just **6%** of the sales, they represent **25%** of the total purchases amount.")
+time.sleep(2)
+print("mean, median and mode of the price in the dataframe")
 print("mean : ",sales_clean.price.mean())
 print("median : ",sales_clean.price.median())
 print("mode : ",sales_clean.price.mode()[0])
 
 
-# We confirm here that most of the sales are between **0 and 16**, with highest sales at **13.9**
-
-# ### **Concentration of sales - Lorenz Curve**
+print("We confirm here that most of the sales are between **0 and 16**, with highest sales at **13.9**")
+time.sleep(2)
+print("Concentration of sales - Lorenz Curve")
 
 
 # Plot the lorenz curve
@@ -152,17 +152,16 @@ time.sleep(2)
 # #### **Gini Coefficient**
 
 
-# compute gini coefficient
+print("compute gini coefficient")
 AUC = (lorenz.sum() - lorenz[-1]/2 - lorenz[0]/2)/n # Area under the curve
 S = 0.5 - AUC # Area between the curve and the 1st bissector
 gini = round(2*S,1)*100
-gini
 
 
-# We have a gini coefficient of around 40%, which means there is inequality.
-# Bottom 84% of the sales just give 60% of the total purchases amount.
+print("""We have a gini coefficient of around 40%, which means there is inequality.
+Bottom 84% of the sales just give 60% of the total purchases amount.""")
 
-# ### **Which is the category with more sales?**
+print("Which is the category with more sales?")
 
 # Barplot of the categories
 sns.countplot(data = sales_clean, x = "category")
@@ -188,11 +187,12 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# **62%** of the sales are in category 0, **33%** in category 1 and just **5.2%** in category 2. 
+print("62\% of the sales are in category 0, 33\% in category 1 and just 5.2\% in category 2.")
+time.sleep(2) 
 
-# ### **What is the Customer's Age with highest purchases**
+print("What is the Customer's Age with highest purchases")
 
-# Plot age distribution
+print("Plot age distribution")
 bins_sales_clean = int(round(1 + np.log2(sales_clean.shape[0])))# Optimal number of bins
 sns.distplot(sales_clean.age, bins = bins_sales_clean) # Distribution plot
 plt.xlabel("Age", fontsize = 18, fontweight = "bold")
@@ -208,9 +208,9 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# ## Bivariate Analysis
+print("Bivariate Analysis")
 
-# ### **What is the Average price in each category?**
+print("What is the Average price in each category?")
 
 # Considering all the dataframe
 # sns.catplot(x = "price", data = sales_clean, kind = "box", col = "category");
@@ -222,15 +222,15 @@ for i in range(3):
     print("Standard Deviation Category " + str(i) + ":", sales_clean.price[sales_clean.category == str(i)].std(),"\n")    
 
 
-# The average price in category `0 is $10, in category 1 it is $19 and in category 2 it is $62`. 
-# There are many outliers in all categories. we take as average the median.
+print("""The average price in category `0 is $10, in category 1 it is $19 and in category 2 it is $62`. 
+There are many outliers in all categories. we take as average the median.""")
 
 
 # Average sales in each category removing outliers
 # sns.catplot(x = "price", data = sales_clean_no_outliers, kind = "box", col = "category")
 # # plt.show();
 
-# measures of center df without outliers
+print("measures of center df without outliers:")
 for i in range(3):
     print("Mean Category " + str(i) + ": ", sales_clean_no_outliers.price[sales_clean_no_outliers.category == str(i)].mean())
     print("Median Category " + str(i) + ": ", sales_clean_no_outliers.price[sales_clean_no_outliers.category == str(i)].median())
@@ -238,11 +238,10 @@ for i in range(3):
     print("Standard Deviation Category " + str(i) + ":", sales_clean_no_outliers.price[sales_clean_no_outliers.category == str(i)].std(),"\n")
 
 
-# It seems that all the outiers values are from product of category 2. Product of category 2 are the most expensives.
-
-# Most of the sales come from people aged between 30 and 55 with a peak at 42 years old.
+print("It seems that all the outiers values are from product of category 2. Product of category 2 are the most expensives.")
+print("Most of the sales come from people aged between 30 and 55 with a peak at 42 years old.")
 # 
-# ### **Sales by Gender**
+print("Sales by Gender")
 
 # Barplot of the categories
 sns.countplot(data = sales_clean, x = "gender")
@@ -269,48 +268,86 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# The number of sales is almost the same for each gender.
+print("The number of sales is almost the same for each gender.")
 
-# ### **Sales over time**
+print("Sales over time")
 
 # Check all the date availaible.
 
 # Check the years 
 sales_clean.date.dt.year.unique()
 
-
 # Check the last date of 2022
 sales_clean.date.max()
 
-
 # We have data for 2021 and 2022. For 2022 we have just January and February.
 
-
-# Time series of the price each month just for 2021
+print("Time series of the price each month") 
 sales_over_time = sales_clean.copy()
-sales_over_time["year"] = sales_over_time.date.dt.year # Create year variable
-sales_over_time["month"] = sales_over_time.date.dt.month # Create month variable
+sales_over_time["year"] = sales_over_time.date.dt.strftime("%Y") # Create year variable
+sales_over_time["month"] = sales_over_time.date.dt.strftime("%b")# Create month variable
+sales_over_time.month = pd.Categorical(sales_over_time.month, ordered=True, 
+               categories=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
 sales_over_time.groupby(["year", "month"]).price.sum().plot()# Plot sales over time
 plt.title("Total Purchases Over Time", fontsize = 24, fontweight = "bold", fontstyle = "italic")
 plt.xlabel("Date (2022 & 2021)", fontsize = 18, fontweight = "bold")
 plt.ylabel("Purchases", fontsize = 18, fontweight = "bold")
+plt.xticks(ticks=[2,3,4,5,6,7,8,9,10,11,12,13], 
+           labels=["Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec","Jan", "Feb"], fontsize = 14)
 plt.annotate(s="There is a huge drop in sales in October 2021",
              fontsize = 16,
-             xy=(7,320000),
-             xytext=(0,400000),
+             xy=(9,320000),
+             xytext=(2,400000),
              arrowprops={"arrowstyle":"->", "color":"red"})
-plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
-plt.show()
-plt.savefig("images/sales_over_time.png");
+plt.savefig("images/sales_over_time.svg");
 print("The figure has been saved in images folder")
 time.sleep(2)
 
+print("Monthly growth rate")
+# Compute the monthly sales, grouping by year and month, drop the missing values after grouping
+monthly_sales = sales_over_time.groupby(["year","month"]).price.sum().reset_index().dropna()
+# Compute the growth rate using the pct_change attribute, multiply by 100 to have the percentage
+monthly_sales["growth_rate(%)"] = round(monthly_sales.price.pct_change()*100)
+# Drop the price column
+monthly_growth_rate = monthly_sales.drop(columns="price")
+# Function to plot a custom table, Taken from stackoverflow 
+#(https://stackoverflow.com/questions/26678467/export-a-pandas-dataframe-as-a-table-image)
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    """This function will plot a custom table from a dataframe"""
+    # Remove the plot figure as we just want the table
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+    # Plot the table
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+    # Disable the automatic fontsize
+    mpl_table.auto_set_font_size(False)
+    # Set the fontsize
+    mpl_table.set_fontsize(font_size)
+    # Set the properties of the rows
+    for k, cell in  six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
+# Plot the table
+render_mpl_table(monthly_growth_rate)
+# Save the figure in the image folder
+plt.savefig("images/monthly_growth_rate_table.svg")
+print("The figure has been saved in the images folder")
+average_growth_rate = monthly_growth_rate["growth_rate(%)"].mean()
+print("On average we have a monthly growth rate of "+average_growth_rate+"\% even considering the huge drop ins sales of October\n.\
+let's go deep to see what is really happening")
 
-# In 2021 December is the month with the most sales, while October is the worse. Overall february is the month with the most sales.
-# There is a high drop in october, let's go deep to see what is really happening.
-
-# #### **Sales in October 2021**
+print("Sales in October 2021")
 
 # Compute the dataframe with just october
 sales_clean_oct = sales_clean[sales_clean.date.dt.month == 10]
@@ -336,7 +373,7 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# The Sales in October are very low for the category 1, let's go deep by checking each day of that month.
+print("The Sales in October are very low for the category 1, let's go deep by checking each day of that month.")
 
 # Dataframe of all the days of october, we group by category and days
 sales_oct = sales_clean_oct.groupby(["category", sales_clean_oct.date.dt.day]).sum().reset_index()
@@ -353,11 +390,9 @@ plt.savefig("images/october_days.png");
 print("The figure has been saved in images folder")
 time.sleep(2)
 
-# We can clearly see that from the 2th to the 27th there were no sales. Maybe there was a lack of product that period, this is to investigate.
+print"We can clearly see that from the 2th to the 27th there were no sales. Maybe there was a lack of product that period, this is to investigate.")
 
-# #### **What is the day of the week with the most sales over time?**
-
-
+print("What is the day of the week with the most sales over time?")
 # Group by day and plot
 sales_clean.groupby(sales_clean.date.dt.dayofweek).price.sum().plot(kind = "bar")
 plt.xlabel("Day of Week (2021-2022)", fontsize = 18, fontweight = "bold")
@@ -372,11 +407,11 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# All the days of the week have almost the same amount of purchases, so there is not a better day for sales.
+print("All the days of the week have almost the same amount of purchases, so there is not a better day for sales.\n\n")
 
-# ## Correlations
+print("Correlations\n")
 
-# ### **Correlation between Gender and Category**
+print("Correlation between Gender and Category")
 
 # boxplot, gender and categories of products purchased
 # sns.catplot(x = "gender", y = "price", col = "category", kind = "box" , data = sales_clean)
@@ -424,10 +459,7 @@ print("""There are more sales in category 0 and very few in category 2, even if 
  In category **1** *Female* make more purchases than *Male*.  
  In category **2** *Male* make more purchases than *Female*.""") 
 
-# ### **Correlation between age and total amount of purchases**
-
-
-
+print("Correlation between age and total amount of purchases")
 
 # Create a dataframe with age and the total amount purchases
 corr_age_price = sales_clean.groupby("age").sum().reset_index()
@@ -446,13 +478,10 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# There is a strong negative correlation between the age and the amount of purchases, as the age increases the amount of purchases decreases.  
-# Young people buy more than old people. 
+print("""There is a strong negative correlation between the age and the amount of purchases, as the age increases the amount of purchases decreases.  
+Young people buy more than old people.""")
 
-# ### **Correlation between Age and the Purchase frequency(Number of purchases per month)**
-
-
-
+print("Correlation between Age and the Purchase frequency(Number of purchases per month)")
 
 # Number of purchases per month
 age_vs_month = sales_clean.copy()
@@ -477,14 +506,14 @@ print("The figure has been saved in images folder")
 time.sleep(2)
 
 
-# There is a correlation between the age and the number of purchases per month, as the age increases the number of purchases per month decreases.  
-# The number of purchases per month is higher for customers between 30 and 50 years old. 
+print("""There is a correlation between the age and the number of purchases per month, as the age increases the number of purchases per month decreases.  
+The number of purchases per month is higher for customers between 30 and 50 years old.""")
 
-# ### **Correlation between Age and the Categories of purchases products**
+print("Correlation between Age and the Categories of purchases products")
 
-# Our hypothesis.
-#     `H0 : Age and Category are independant`  
-#     `H1: Age and Category are dependant`
+print("""Our hypothesis:
+     `H0 : Age and Category are independant`  
+     `H1: Age and Category are dependant`""")
 
 
 # Compute a group_age variable
@@ -525,7 +554,7 @@ for category in categories:
     list_category.append(sales_clean[sales_clean.category == category].age)
 plt.boxplot(list_category, labels=categories, vert=False)
 plt.show();
-
+# Function to compute eta squared
 def eta_squared(x,y):
     mean_y = y.mean()
     categories = []
@@ -542,7 +571,7 @@ eta_squared(sales_clean.category, sales_clean.age)
 print("For all ages there are more sales in category 0 and less in category 2.  \n\
 people aged between 35-60 make more purchases in category 0 and 1 compared to other age group.")
 
-# **Correlation between age and the average basket size (in number of items)**
+print("Correlation between age and the average basket size (in number of items)")
 # compute the average basket size
 age_average_basket = sales_clean.groupby(["age", "session_id"]).count().reset_index().groupby("age")["id_prod"].mean().reset_index()
 sns.regplot("age", "id_prod", data = age_average_basket, ci = False)
@@ -558,9 +587,8 @@ plt.savefig("images/age_basket_size.png");
 print("The figure has been saved in images folder")
 time.sleep(2)
 
-# **Using OLS(ordinary Leasts Squares)**
+print("Using OLS(ordinary Leasts Squares)")
 
-import statsmodels.api as sm
 Y = age_average_basket.id_prod # Create a series with the average basket size 
 X = age_average_basket[["age"]] # Create a dataframe with the age column
 X = X.copy()  # Make a copy as the dataframe will be modify
